2ad0e374bb858859a016636340103a948fc9d2e5
ivosam1212 <isam-alv@student.42berlin.de>
2025-04-22 17:43:30 +0200

    Challenge functions beginning

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,10 @@
-package org.example
+
+
+// write a function that satisfies the following requirements
+
+// function takes 2 strings for first and last name
+// function takes a formatting function parameter
+// function should print the result of the passed format function
 
 fun main() {
 
