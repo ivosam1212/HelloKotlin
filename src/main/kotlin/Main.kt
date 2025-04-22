79b0e5e781fc916e6a572b582247397c78c4ea7a
ivosam1212 <isam-alv@student.42berlin.de>
2025-04-22 14:48:47 +0200

    Mutable and immutable data types

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,13 @@
 package org.example
 
 fun main() {
+    var ourFirstVariable = true
 
+    println(ourFirstVariable)
+
+    ourFirstVariable = false
+    println(ourFirstVariable)
+
+    println(ourFirstVariable == true)
+    println(ourFirstVariable == false)
 }
