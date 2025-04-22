c551c3241ce8fe1f5e9ecc5382a24a9d075bfb9c
ivosam1212 <isam-alv@student.42berlin.de>
2025-04-22 19:13:28 +0200

    Challenge solved

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,6 +6,23 @@
 // function takes a formatting function parameter
 // function should print the result of the passed format function
 
-fun main() {
+fun printFormattedName(firstName: String, lastName: String, formatter: (String, String) -> String) {
+    println(formatter(firstName, lastName))
+}
 
+val basicFormatter: (String, String) -> String = { firstName, lastName ->
+    "$firstName $lastName"
+}
+
+val fancyFormatter: (String, String) -> String = { firstName, lastName ->
+    "First name is: $firstName, and last name is: $lastName"
+}
+
+fun main() {
+    printFormattedName("Ivo", "Sam", basicFormatter)
+    printFormattedName("Ivo", "Sam", fancyFormatter)
+
+    printFormattedName("Ivo", "Sam") { firstName, lastName ->
+        "$lastName, $firstName"
+    }
 }
