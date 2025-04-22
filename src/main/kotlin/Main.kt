09094064292d1398857b4faee59c79a1a5f64caf
ivosam1212 <isam-alv@student.42berlin.de>
2025-04-22 14:50:31 +0200

    Empty project

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,5 @@
 package org.example
 
 fun main() {
-    var ourFirstVariable = true
 
-    println(ourFirstVariable)
-
-    ourFirstVariable = false
-    println(ourFirstVariable)
-
-    println(ourFirstVariable == true)
-    println(ourFirstVariable == false)
 }
