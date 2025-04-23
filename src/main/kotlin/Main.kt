752601c8c7dca55681278472702ba661e47de650
ivosam1212 <isam-alv@student.42berlin.de>
2025-04-23 17:50:28 +0200

    Foor loops

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,8 @@
+import kotlin.concurrent.fixedRateTimer
 
 fun main() {
-
+    for (i in 0 .. 5) println(i)
+    for (i in 0 until 5) println(i)
+    for (i in 10 downTo 0) println(i)
+    for (i in 10 downTo 0 step 3) println(i)
 }
