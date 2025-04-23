eab81b85c97d7e94e86d2c44e72e9a7936f7cbeb
ivosam1212 <isam-alv@student.42berlin.de>
2025-04-23 17:03:19 +0200

    Try/catch as control flow

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,12 @@
 
 fun main() {
-
+    val message = try {
+        throw IllegalStateException()
+        "The value is ${10 / 0}"
+    } catch (error: ArithmeticException) {
+        "Error was thrown"
+    } catch (error: java.lang.IllegalStateException){
+        "Error was IllegalState"
+    }
+    println(message)
 }
