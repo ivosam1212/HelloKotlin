11ad51a16ada3a6539b2f4d877742d9733318a28
ivosam1212 <isam-alv@student.42berlin.de>
2025-04-23 17:43:33 +0200

    while loops

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,15 @@
 
 fun main() {
+    var counter = 0
 
+    /*while (counter < 5) {
+        println(counter)
+        counter++
+        if (counter == 3) break
+    }*/
+
+    do {
+        println(counter)
+        counter += 1
+    } while (counter < 5)
 }
