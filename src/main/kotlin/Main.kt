a2d19ef71849a01b2216cd5fae913acfd487ebc1
ivosam1212 <isam-alv@student.42berlin.de>
2025-04-23 17:22:37 +0200

    Smartcast

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,29 @@
 
-fun main() {
+fun checkType(input: Any?) {
+/*    if (input is String) {
+        if (input == null) {
+            println("Input was a null String")
+        } else {
+            println("Input is a String with a length ${input.length}")
+        }
+    }
 
+    if (input !is Int) {
+        println("Input is not an Int")
+    }
+
+    if (input is Int) {
+        println("Input is an Int")
+    }*/
+    when (input) {
+        null -> println("Input was null")
+        is String -> println("Input was a String of length ${input.length}")
+    }
+
+}
+
+fun main() {
+    val aGenericVariable: Any = "A String"
+
+    checkType(aGenericVariable)
 }
