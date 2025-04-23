13676f92c13ea6b446460f9b005523ba71315ce7
ivosam1212 <isam-alv@student.42berlin.de>
2025-04-23 12:48:56 +0200

    Statements vs expressions

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,18 @@
+fun getMessage(input: Int) = if (input >3) {
+        "Greater than 3"
+    } else {
+        "Not greater than 3"
+    }
+
+fun getMessageWithWhen(input: Int) = when (input) {
+    3 -> "Value is 3"
+    else -> "Value is not 3"
+}
 
 fun main() {
-
+    val someVariable = 0
+    val message = when (someVariable) {
+        3 -> "The value is 3"
+        else -> "The value is not 3"
+    }
 }
