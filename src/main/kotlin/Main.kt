b05757c892caa9e524168407add76b218bd733ec
ivosam1212 <isam-alv@student.42berlin.de>
2025-04-24 15:34:55 +0200

    Evalute expressions and add watches

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,8 +4,8 @@ fun getRandomInt() = Random.nextInt()
 
 fun main() {
     for (i in 0..4) {
-        val random = getRandomInt()
-        val isEven = random.rem(2) == 1
+        val random = Random.nextInt()
+        val isEven = random.rem(2) == 0
         when(isEven) {
             true -> println("$random is even")
             false -> println("$random is odd")
