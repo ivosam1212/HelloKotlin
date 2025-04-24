ce97a203f400d68ecf64e0c711664f3b36f0d7b9
ivosam1212 <isam-alv@student.42berlin.de>
2025-04-24 15:06:25 +0200

    Beginning of 05_01

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,13 @@
+import kotlin.random.Random
 
 fun main() {
+    for (i in 0..4) {
+        val random = Random.nextInt()
+        val isEven = random.rem(2) == 1
+        when(isEven) {
+            true -> println("$random is even")
+            false -> println("$random is odd")
+        }
+    }
 
 }
