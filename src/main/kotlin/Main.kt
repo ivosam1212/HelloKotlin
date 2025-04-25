83ac377127ff964951ce1687a87c3657eccf7cbe
ivosam1212 <isam-alv@student.42berlin.de>
2025-04-25 12:23:32 +0200

    challenge: Filtering a list of data

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,4 @@
 
 fun main() {
-    val languages = listOf("kotlin", "c++", "java")
-        .filter { it.length > 4 }
-        .map {it.length }
 
-    val languageSequence = sequenceOf("kotlin", "c++", "java")
-
-    val fromIterable = listOf(1, 2, 3).asSequence()
-
-    languageSequence.filter { it.length > 4 }.map { it.length }.take(2)
 }
