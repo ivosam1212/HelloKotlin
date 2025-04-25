7b9d1c9479f5c8de4e7c1f67bb1cd79afb749b26
ivosam1212 <isam-alv@student.42berlin.de>
2025-04-25 12:54:35 +0200

    Challenge solved

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,18 @@
 
 fun main() {
-
+    testScores.toList()
+        .sortedBy { it.second }
+        .map { it.first }
+        .take(3)
+        .forEach { println(it)}
 }
+
+/* Another solution
+Convert map to a list of pairs
+    val scoresList = testScores.toList()
+
+Sort by score (second element in each pair)
+    val lowerScores = scoresList.sortedBy { it.second }
+        .take(3)
+        .forEach { println(it.first) }  // Print just the ID
+ */
