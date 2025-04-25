53f18edf5dc10383498a8a7eb88676cf574665db
ivosam1212 <isam-alv@student.42berlin.de>
2025-04-25 16:45:19 +0200

    Writing JUnit tests

---
+++ src/test/kotlin/RandomNumberGeneratorTest.kt
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+class RandomNumberGeneratorTest {
+
+    @Test
+    fun verifyTrueIsAlwaysTrue() {
+        assert(true == true)
+    }
+
+    @Test
+    fun `getRandom returns value between 0 and 100`() {
+        val random = getRandom(100)
+        assert(random in 0 .. 100)
+    }
+
+    @Test
+    fun `getRandom throws exception when negative value is passed`() {
+        assertThrows<IllegalArgumentException> {
+            getRandom(-1)
+        }
+    }
+}
